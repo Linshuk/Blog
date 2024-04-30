3c2492c6e4ff07f3d5957d935f85ce4d5822d59b
Linshuk <102722374+Linshuk@users.noreply.github.com>
2024-04-30 20:13:07 +0800

    导出分类

--- admin/src/main/java/jmu/lsk/controller/CategoryController.java
+++ admin/src/main/java/jmu/lsk/controller/CategoryController.java
@@ -1,13 +1,21 @@
 package jmu.lsk.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import jmu.lsk.domain.ResponseResult;
+import jmu.lsk.domain.entity.Category;
 import jmu.lsk.domain.vo.CategoryVo;
+import jmu.lsk.domain.vo.ExcelCategoryVo;
+import jmu.lsk.enums.AppHttpCodeEnum;
 import jmu.lsk.service.CategoryService;
+import jmu.lsk.utils.BeanCopyUtils;
+import jmu.lsk.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -22,5 +30,24 @@ public class CategoryController {
         return ResponseResult.okResult(list);
     }
 
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        try {
+            //设置下载文件的请求头
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            //获取需要导出的数据
+            List<Category> categoryVos = categoryService.list();
+
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            //把数据写入到Excel中
+            EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出")
+                    .doWrite(excelCategoryVos);
+
+        } catch (Exception e) {
+            //如果出现异常也要响应json
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
+    }
     
 }
